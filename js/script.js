8dee56eee2ecfc335097e04d0316f3feadfe6989
kevinw123 <kevinwong_895@hotmail.com>
2016-12-19 15:36:51 -0800

    fixing results after submit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function submitAnswers(){
 	var total = 5;
 	var score = 0;
-
 	// Get User Input
 	var q1 = document.forms["quizForm"]["q1"].value;
 	var q2 = document.forms["quizForm"]["q2"].value;
 	var q3 = document.forms["quizForm"]["q3"].value;
 	var q4 = document.forms["quizForm"]["q4"].value;
 	var q5 = document.forms["quizForm"]["q5"].value;
-
+	console.log(q1);
 	// Validation
 	for(var i = 1; i <= total; i++){
 		if(eval('q'+i) == null ||  eval('q'+i) == ''){
